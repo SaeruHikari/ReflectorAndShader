bc571ca20b4031edf35cdceba6518aff09181889
SaeruHikari <3045349520@qq.com>
2025-05-03 01:05:01 +0800

    fix clang compile

--- shader/xmake.lua
+++ shader/xmake.lua
@@ -9,7 +9,7 @@ target("SSLLLVM")
     set_kind("static")
     set_runtimes("MD")  -- runtime depend on LLVM compiled version, official version is MT
     set_languages("cxx20")
-    add_deps("libTooling", "SSLAST")
+    add_deps("libTooling", "SSLAST", {public = true})
     add_includedirs("LLVM/include", {public = true})
     add_files("LLVM/src/**.cpp")
 
